bc587312533f4dc8984997e4e051c9a1d79bca8c
JohnCdev <johncarverp@gmail.com>
2019-09-15 18:12:52 -0400

    couple fun extra animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
-var navIcon = $("<div>");
-
-
 $(document).ready(function () {
     $('.parallax').parallax();
+
+    $("#name-animation").click(function () {
+        console.log("clicked");
+        $("#name-animation").addClass("shake");
+        $("#name-animation").css("text-shadow", "0 0 30px white");
+        setTimeout(function () {
+            $("#name-animation").removeClass("shake");
+            $("#name-animation").css("text-shadow", "");
+        }, 500);
+    });
 });
+
+
+
